ca76ea7ee413767d545cb459b6243b20c2585f5c
Neylkson <neylkson.pereira@fatec.sp.gov.br>
2021-06-20 21:37:41 -0300

    Additional

--- Lógica_de_Programação_deixando_os_seus_programas_espertos/Exercises.py
+++ Lógica_de_Programação_deixando_os_seus_programas_espertos/Exercises.py
@@ -1,5 +1,8 @@
 #Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10! 
 
+from ast import If
+
+
 X = int(input("Digite um número: "))
 
 if (X > 10):
@@ -26,3 +29,27 @@ if v_1 > v_2:
     print(v_1, v_2)
 else:
     print(v_2, v_1)
+
+#Ler um valor e escrever se é positivo, negativo ou zero.
+
+v1 = int(input("Digite um valor: "))
+
+if v1 < 0:
+    print("Negativo")
+if v1 > 0:
+    print("Positivo")
+else:
+    print("Neutro")
+
+#Ler 3 valores (considere que não serão informados valores iguais) e escrever o maior deles.
+
+number1 = int(input("Digite um valor: "))
+number2 = int(input("Digite outro valor:"))
+number3 = int(input("Digite mais um valor: "))
+
+if number1 > number2 and number1 > number3:
+    print(number1)
+if number2 > number1 and number2 > number3:
+    print(number2)
+else:
+    print(number3)
