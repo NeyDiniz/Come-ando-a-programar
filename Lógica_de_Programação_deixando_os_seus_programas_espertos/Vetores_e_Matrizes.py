a2972edba411cc0dde26390280bf7af4e6b474b4
Neylkson <neylkson.pereira@fatec.sp.gov.br>
2021-06-22 11:13:46 -0300

    Create Vetores_e_Matrizes.py

---
+++ Lógica_de_Programação_deixando_os_seus_programas_espertos/Vetores_e_Matrizes.py
@@ -0,0 +1,42 @@
+#Faça um programa que contenha um vetor de 4 posições. Preencha as posições com os valores 5, 9, 20, 5. Exiba o valor contido na posição 2.
+
+vector = [5, 9, 20, 5]
+
+print("O vetor na posição 2 é o", vector[2])
+
+#Faça um programa que crie um vetor para abrigar 5 nomes de pessoas. Também crie uma variável que guardará a posição de um vetor. Depois, indique que a posição é 3 e que nesta posição deve ser armazenado o nome Amanda. Exiba na tela o conteúdo da posição corrente.
+
+nomes = ["Maria", "Júlia", "Stella", "Cecília", "Rúbi"]
+
+nomes[3] = "Amanda"
+
+print("O nome na posição 3 é", nomes[3])
+
+#Faça um programa que crie um vetor de 10 números inteiros. Em seguida, peça para o usuário informar os 10 valores. Após isso, exiba os 10 valores (um por linha)
+
+myvector = [0,1,2,3,4,5,6,7,8,9]
+
+for i in range(0, 10):
+    myvector[i] = myvector[i] * 2
+
+print(myvector)
+
+for i in myvector:
+    print(i)
+
+#Escreva um algoritmo que lê um vetor com seis posições e o escreve. Conte, a seguir, quantos valores do vetor são negativos e escreva esta informação.
+
+v = [6, -2, 9, -8, -3, 4]
+
+print(v)
+
+if v < 0:
+    
+
+
+
+    
+
+    
+
+
