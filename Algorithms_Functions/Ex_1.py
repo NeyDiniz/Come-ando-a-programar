147a20aab84faf69c45c1a0723bac3c6abe52c13
Neylkson <neylkson.pereira@fatec.sp.gov.br>
2021-06-22 22:33:28 -0300

    add

--- Algorithms_Functions/Ex_1.py
+++ Algorithms_Functions/Ex_1.py
@@ -1,23 +1,50 @@
 #Construa uma função que calcule o Arranjo de N elementos, p a p. Utilize a fórmula A = N!/(N-P)!
 
-
 def fact(n):
+    i = 1
     f = 1
     while n > 0:
         f = f * n
         n = n - 1
-    return f
+   #return f
+    n = 5
+    print(n)
 
-for i in range(1):
-    print(fact(i))     
+
+# def arranjo():
+#     n = 8
+#     n1 = 4
+
+#     #Inicializar as variaveis
+
+#     a = n - n1
+#     n_fact = 1
+#     i = 1
+#     n_fact1 = 1
+#     i1 = 1
+    
+#     #Calcular fatorial!
+
+#     while i > n:
+#         n_fact = n_fact * 1
+#         i = i - 1
+        
+#     while i1 > n1:
+#         n_fact1 = n_fact1 * i1
+#         i1 = i1 - 1
+            
+#     r = n_fact1/n_fact
+#     print(r)
 
 
-def arranjo(N, P):
-    for n in range(1, N):
-        A = fact(n)/(fact (n - P))
+
+
+
+
+        
 
     
-        print("O valor será", A)
+        
 
    
     
