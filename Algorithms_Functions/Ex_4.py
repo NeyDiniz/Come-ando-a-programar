d384f8c8e3ace37c462e6fd1768fc5a217eeec10
Neylkson <neylkson.pereira@fatec.sp.gov.br>
2021-06-26 20:11:06 -0300

    Create Ex_4.py

---
+++ Algorithms_Functions/Ex_4.py
@@ -0,0 +1,14 @@
+#Criar um programa em Python que leia os elementos de uma matriz inteira 4 x 4 e:
+# Escreva os elementos da diagonal principal;
+# Escreva todos os elementos, exceto os elementos da diagonal principal;
+# Escreva os elementos da diagonal secundária.
+
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0],[0, 0, 0]]
+for l in range(0, 4):
+    for c in range(0, 4):
+        matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}: '))
+print('-' *30)
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+    print()
