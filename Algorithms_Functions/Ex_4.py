bf2894b0a52f132bf1fe7813c080935f1f9e2dda
Neylkson <neylkson.pereira@fatec.sp.gov.br>
2021-06-29 19:17:16 -0300

    Update Ex_4.py

--- Algorithms_Functions/Ex_4.py
+++ Algorithms_Functions/Ex_4.py
@@ -8,7 +8,12 @@ for l in range(0, 4):
     for c in range(0, 4):
         matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
 print('-' *30)
+
+
 for l in range(0, 4):
     for c in range(0, 4):
         print(f'[{matriz[l][c]:^5}]', end='')
+        if l == c:
+            DP
+
     print()
