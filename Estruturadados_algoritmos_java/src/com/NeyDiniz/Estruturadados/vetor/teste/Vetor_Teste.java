6b68010751070eb5356e19774e4935fdf4b8dcd9
NeyDiniz <neylkson.pereira@fatec.sp.gov.br>
2022-02-21 11:35:04 -0300

    add elemento no vetor

---
+++ Estruturadados_algoritmos_java/src/com/NeyDiniz/Estruturadados/vetor/teste/Vetor_Teste.java
@@ -0,0 +1,17 @@
+package com.NeyDiniz.Estruturadados.vetor.teste;
+
+import com.NeyDiniz.Estruturadados.vetor.Vetor;
+
+public class Vetor_Teste {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(5);
+		
+		int i = 0;
+		i++;
+	
+
+	}
+
+}
---
+++ Estruturadados_algoritmos_java/src/com/NeyDiniz/Estruturadados/vetor/teste/Vetor_Teste01.java
@@ -0,0 +1,17 @@
+package com.NeyDiniz.Estruturadados.vetor.teste;
+
+import com.NeyDiniz.Estruturadados.vetor.Vetor;
+
+public class Vetor_Teste01 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(5);
+		
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+	
+
+	}
+
+}
